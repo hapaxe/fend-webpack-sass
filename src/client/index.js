00a3d10e65e5cb87c8d2ee47c79c4727eb4e580c
alexf <alexfremauxgeoten@gmail.com>
2020-04-22 16:07:52 -0700

    install loaders and replace css files with sass files.

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,10 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import './styles/base.sass'
+import './styles/footer.sass'
+import './styles/form.sass'
+import './styles/header.sass'
+import './styles/resets.sass'
 
 console.log(checkForName);
 
